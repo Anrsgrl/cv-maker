e6d0ef0cad0f7aac0dd3a1611dd67fab449fc9d6
Anar Asgarli <esgerlianar003@gmail.com>
2024-05-21 19:09:10 +0400

    build: add TailwindCSS setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1>cv-maker</h1>
+      <h1 className="text-3xl font-bold underline">cv-maker</h1>
       <PWABadge />
     </>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
   plugins: [react(), VitePWA({
     registerType: 'prompt',
     injectRegister: false,
