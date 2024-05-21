ea5506364fa96ca39fd9f7cab97959ea6f6786f6
Anar Asgarli <esgerlianar003@gmail.com>
2024-05-21 19:00:56 +0400

    build: set up initial file structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import PWABadge from './PWABadge.tsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>cv-maker</h1>
+      <PWABadge />
+    </>
+  )
+}
+
+export default App
