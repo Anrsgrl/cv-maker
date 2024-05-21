b032c9a5226ac46f2ae6e06ad69d772802cd492e
Anar Asgarli <esgerlianar003@gmail.com>
2024-05-21 19:59:18 +0400

    feat: add Not Found page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
